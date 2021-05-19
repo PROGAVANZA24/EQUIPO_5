cef0bab956b3b454118180e98eb81d64ba9a522a
Ricardo Chavez Mata <ricardo.chavezmt@uanl.edu.mx>
2021-05-19 04:43:51 -0500

    Se ha agregado el modulo para guardar los datos en un archivo .txt en Temas.py

--- Temas.py
+++ Temas.py
@@ -3,3 +3,9 @@ class Temas:
     def __init__(self, id_tema = 0, nombre = ' '):
         self.__id_tema = id_tema
         self.__nombre = nombre
+
+    def guardar(self):
+        
+        f = open('C:\\Archivos_Progra\\PIA\\EQUIPO_5\\Archivos_TXT\\Temas.txt', 'a', encoding = 'utf8')
+        f.write(f'ID: {self.__id_tema} | TEMA: {self.__nombre}' + '\n')
+        f.close 
