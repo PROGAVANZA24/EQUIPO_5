033cbbac492260299d32c722596c1468b2c12af4
Ricardo Chavez Mata <ricardo.chavezmt@uanl.edu.mx>
2021-05-19 05:27:01 -0500

    Se han integrado los print para mostrar en el Menu Principal

--- Menu_Principal.py
+++ Menu_Principal.py
@@ -4,4 +4,12 @@ from Videos import Videos
 Ciclo_Principal = True
 
 while Ciclo_Principal == True:
-    pass
+    
+    print('---MENU PRINCIPAL---')
+    print('1.-Temas')
+    print('2.-Videos')
+    print('3.-Cursos Temas Videos')
+    print('4.-Cursos Temas')
+    print('5.-Cursos')
+    print('6.-Empleados')
+    print('7.-Salir')
