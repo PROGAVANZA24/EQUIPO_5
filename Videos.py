e6922a1b6d7e759b763b75243ab7098ccd9e7fc1
Ricardo Chavez Mata <ricardo.chavezmt@uanl.edu.mx>
2021-05-19 05:17:12 -0500

    Se ha agregado el inicializador en Videos.py

---
+++ Videos.py
@@ -0,0 +1,7 @@
+class Videos:
+
+    def __init__(self, id_video = 0, nombre = ' ', url = ' ', fecha_publicacion = ' '):
+        self.__id_video = id_video
+        self.__nombre = nombre
+        self.__url = url
+        self.__fecha_publicacion = fecha_publicacion
