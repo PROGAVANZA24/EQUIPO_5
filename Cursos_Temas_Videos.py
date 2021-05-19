516dfc438286340bcbd00c291659b269820e25fe
Josue Daniel Gonzalez Segoviano <josue.sego14@gmail.com>
2021-05-19 15:16:00 -0500

    se acaba de crear la clase y se puso el inicializador

---
+++ Cursos_Temas_Videos.py
@@ -0,0 +1,6 @@
+class Cursos_Temas_Videos:
+    
+    def __init__(self, id_CTV = 0, id_CT = 0, id_video = 0):
+        self.__id_CTV = id_CTV
+        self.__id_CT = id_CT
+        self.__id_video = id_video
