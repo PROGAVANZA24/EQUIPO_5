5f2e714cfd41666adbe942fe054870deae7da079
SeiMei-16 <lealdavidesc@gmail.com>
2021-05-19 18:04:27 -0500

    Se agrego el metodo para guardar en Empleados.py

--- Empleados.py
+++ Empleados.py
@@ -4,3 +4,9 @@ class Empleados:
         self.__id_empleado = id_empleado
         self.__nombre = nombre
         self.__direccion = direccion
+
+    def guardar(self):
+        
+        f = open('C:\\Users\\richa\\Desktop\\PIA_Prueba\\Archivos de Texto\\Empleados.txt', 'a', encoding = 'utf8')
+        f.write(f'ID: {self.__id_empleado} | NOMBRE: {self.__nombre} | DIRECCION: {self.__direccion}' + '\n')
+        f.close
