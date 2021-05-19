08178e0985129aecce2fa35141aedc7036ccd25f
Josue Daniel Gonzalez Segoviano <josue.sego14@gmail.com>
2021-05-19 15:19:40 -0500

    se acaba de crear el metodo de consultar todo de la clase cursos temas videos

--- Cursos_Temas_Videos.py
+++ Cursos_Temas_Videos.py
@@ -10,3 +10,9 @@ class Cursos_Temas_Videos:
         f = open('C:\\Users\\richa\\Desktop\\PIA_Prueba\\Archivos de Texto\\Cursos_Temas_Videos.txt', 'a', encoding = 'utf8')
         f.write(f'ID CURSO-TEMA-VIDEO: {self.__id_CTV} | ID CURSO-TEMA: {self.__id_CT} | ID VIDEO: {self.__id_video}' + '\n')
         f.close
+
+    def consultar_todo(self):
+
+        f = open('C:\\Users\\richa\\Desktop\\PIA_Prueba\\Archivos de Texto\\Cursos_Temas_Videos.txt')
+        print (f.read())
+        f.close
