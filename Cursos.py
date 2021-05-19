e9e73fab46726f68770a807151ee8b760d2b450c
Josue Daniel Gonzalez Segoviano <josue.sego14@gmail.com>
2021-05-19 17:22:01 -0500

    se agrego el inicializador a la clase de cursos

---
+++ Cursos.py
@@ -0,0 +1,6 @@
+class Cursos:
+    
+    def __init__(self, id_curso = 0, descripcion = ' ', id_empleado = 0):
+        self.__id_curso = id_curso
+        self.__descripcion = descripcion
+        self.__id_empleado = id_empleado
