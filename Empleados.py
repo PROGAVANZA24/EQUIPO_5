a185b285cf0bb8cb54319d7128144e81d0ef1754
SeiMei-16 <lealdavidesc@gmail.com>
2021-05-19 18:00:23 -0500

    Se creo la clase empleados.py y se agrego el inicializador

---
+++ Empleados.py
@@ -0,0 +1,6 @@
+class Empleados:
+    
+    def __init__(self, id_empleado = 1, nombre = ' ', direccion = ' '):
+        self.__id_empleado = id_empleado
+        self.__nombre = nombre
+        self.__direccion = direccion
