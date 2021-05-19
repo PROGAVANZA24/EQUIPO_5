3175a05139579cf862411e83badfde728860b142
Josue Daniel Gonzalez Segoviano <josue.sego14@gmail.com>
2021-05-19 17:23:44 -0500

    se agrego el modulo de guardar en la clase de cursos

--- Cursos.py
+++ Cursos.py
@@ -4,3 +4,9 @@ class Cursos:
         self.__id_curso = id_curso
         self.__descripcion = descripcion
         self.__id_empleado = id_empleado
+
+    def guardar(self):
+        
+        f = open('C:\\Users\\richa\\Desktop\\PIA_Prueba\\Archivos de Texto\\Cursos.txt', 'a', encoding = 'utf8')
+        f.write(f'ID: {self.__id_curso} | DESCRIPCION: {self.__descripcion} | EMPLEADO: {self.__id_empleado}' + '\n')
+        f.close
